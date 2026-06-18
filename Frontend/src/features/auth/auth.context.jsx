cb9e7934408b20bf7b2d9149f24b2e9df2af25bc
Himani <himaniinegii482@gmail.com>
2026-06-18 13:18:18 +0530

    Fixed auth api import path

--- Frontend/src/features/auth/auth.context.jsx
+++ Frontend/src/features/auth/auth.context.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState,useEffect } from "react";
-import {getMe } from "./services/interview.api.js";
+import {getMe } from "./services/auth.api.js";
 
 export const AuthContext = createContext()
 
